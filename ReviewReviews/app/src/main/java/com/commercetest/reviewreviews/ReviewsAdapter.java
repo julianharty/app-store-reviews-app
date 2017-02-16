16c2be5f51748a6770bd9e9d7c070e5bc37e2b77
Julian Harty <julianharty@gmail.com>
2017-02-16 21:36:36 +0000

    Finally, the first version with the RecyclerView for the Triage process.
    The Triage process doesn't work yet. I need to add the capability. I also need to consider how to interact with the database correctly so we don't lose memory or have performance issues. And the GUI is very unattractive. Nonetheless this is a significant step in the right direction, finally. Phew!

--- ReviewReviews/app/src/main/java/com/commercetest/reviewreviews/ReviewsAdapter.java
+++ ReviewReviews/app/src/main/java/com/commercetest/reviewreviews/ReviewsAdapter.java
@@ -1,38 +1,71 @@
 package com.commercetest.reviewreviews;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.support.v7.widget.RecyclerView;
 
 /**
+ * Adapter for the RecyclerView used to display reviews from the Google Play Store.
+ *
+ * Still very much a work-in-progress.
  * Created by julianharty_air on 07/12/2016.
  */
 
-public class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ViewHolder> {
+class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ViewHolder> {
 
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView reviewTextView;
+        Button undoButton;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            reviewTextView = (TextView) itemView.findViewById(R.id.review_text);
+            undoButton = (Button) itemView.findViewById(R.id.undo_button);
+        }
+    }
+
+    private Cursor mCursor;
+
+    ReviewsAdapter(Cursor cursor) {
+        mCursor = cursor;
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
 
+        // Inflate the layout for an individual App Store review
+        View reviewView = inflater.inflate(R.layout.review_row, parent, false);
+        return new ViewHolder(reviewView);
+    }
+
+    @Override
+    public void onBindViewHolder(ReviewsAdapter.ViewHolder holder, int position) {
+        // Get the data model based on the position
+        mCursor.moveToPosition(position);
+        String reviewTitle = mCursor.getString(mCursor.getColumnIndexOrThrow("review_title"));
+        int rating = mCursor.getInt(mCursor.getColumnIndexOrThrow("star_rating"));
+
+        if (reviewTitle == null || reviewTitle.length() == 0) {
+            reviewTitle = "(none) rating = " + rating;
+        }
+
+        // Update the row with the details from this review
+        TextView textView = holder.reviewTextView;
+        textView.setText(reviewTitle);
+        Button button = holder.undoButton;
+        button.setText("UnDo"); // Needs translating, etc.
     }
 
     @Override
     public int getItemCount() {
-        return 0;
-    }
-
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView reviewTextView;
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-
-            reviewTextView = (TextView) itemView.findViewById(R.id.review_text);
-        }
+        return mCursor.getCount();
     }
 }
--- ReviewReviews/app/src/main/java/com/commercetest/reviewreviews/TriageActivity.java
+++ ReviewReviews/app/src/main/java/com/commercetest/reviewreviews/TriageActivity.java
@@ -1,13 +1,51 @@
 package com.commercetest.reviewreviews;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 public class TriageActivity extends AppCompatActivity {
+    RecyclerView rvReviews;
+    private final String TAG = "TriageActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_triage);
+        rvReviews = (RecyclerView) findViewById(R.id.reviews);
+
+        Cursor cursor = readFromDB();
+        ReviewsAdapter adapter = new ReviewsAdapter(cursor);
+
+        rvReviews.setAdapter(adapter);
+        // TODO 20170216 (jharty) see if we can define the layout mgr in XML as Google recommends
+        rvReviews.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private Cursor readFromDB() {
+        SQLiteDatabase db = ReviewsDatabaseHelper.getDatabase(this);
+
+        String[] projection = {
+                "_id",
+                "star_rating",
+                "review_submitted",
+                "review_title",
+                "review_text"
+        };
+
+        Cursor cursor = db.query(ReviewsDatabaseHelper.GOOGLE_PLAY_REVIEW,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null,
+                "1000");
+        Log.d(TAG, "The cursor count is " + cursor.getCount());
+        return cursor;
     }
 }
