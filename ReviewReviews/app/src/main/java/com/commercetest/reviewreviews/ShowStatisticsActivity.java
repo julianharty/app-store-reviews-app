6da4111a82a0fd15a72d22ee80aa85ee409bdee4
Julian Harty <julianharty@gmail.com>
2017-02-20 21:44:32 +0000

    Added additional details about the file imports.
    Still pretty basic, but getting better. This code could usefully be cleaned up, I expect.

--- ReviewReviews/app/src/main/java/com/commercetest/reviewreviews/ShowStatisticsActivity.java
+++ ReviewReviews/app/src/main/java/com/commercetest/reviewreviews/ShowStatisticsActivity.java
@@ -1,5 +1,6 @@
 package com.commercetest.reviewreviews;
 
+import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +9,8 @@ import android.widget.TextView;
 
 import static com.commercetest.reviewreviews.DatabaseConstants.FILE_IMPORT;
 import static com.commercetest.reviewreviews.DatabaseConstants.GOOGLE_PLAY_REVIEW;
+import static com.commercetest.reviewreviews.DatabaseConstants.NUM_ACCEPTED;
+import static com.commercetest.reviewreviews.DatabaseConstants.NUM_REJECTED;
 
 /**
  * Shows statistics to the user.
@@ -32,7 +35,26 @@ public class ShowStatisticsActivity extends AppCompatActivity {
 
         totalFilesImported = (TextView) findViewById(R.id.total_files_imported);
         long imports = DatabaseUtils.queryNumEntries(db, FILE_IMPORT);
-        totalFilesImported.setText(String.format("%,d", imports));
+
+        String[] projection = {
+                "SUM(" + NUM_ACCEPTED + ") AS ACCEPTED_COUNT",
+                "SUM(" + NUM_REJECTED + ") AS REJECTED_COUNT"
+        };
+        Cursor statistics = db.query(DatabaseConstants.FILE_IMPORT,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null);
+        statistics.moveToFirst();
+        long accepted = statistics.getInt(statistics.getColumnIndexOrThrow("ACCEPTED_COUNT"));
+        long rejected = statistics.getInt(statistics.getColumnIndexOrThrow("REJECTED_COUNT"));
+        totalFilesImported.setText(
+                String.format("files %,d, accepted %,d, rejected %,d",
+                    imports, accepted, rejected));
+
     }
 
 
