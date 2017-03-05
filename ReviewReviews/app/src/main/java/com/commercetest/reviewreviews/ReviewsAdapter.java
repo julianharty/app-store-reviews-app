f2fb2e629256a2f9bd4634e94c314085eb5d5754
Julian Harty <julianharty@gmail.com>
2017-03-05 12:37:39 +0000

    Various attempts to improve the GUI somewhat.
    I'd dearly appreciate someone who's able to help improve the UI appearance BTW :)

--- ReviewReviews/app/src/main/java/com/commercetest/reviewreviews/ReviewsAdapter.java
+++ ReviewReviews/app/src/main/java/com/commercetest/reviewreviews/ReviewsAdapter.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.support.v7.widget.RecyclerView;
 
+import org.w3c.dom.Text;
+
 /**
  * Adapter for the RecyclerView used to display reviews from the Google Play Store.
  *
@@ -20,13 +22,17 @@ class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ViewHolder>
     implements View.OnClickListener {
 
     static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView reviewRatingView;
         TextView reviewTextView;
+        TextView reviewTitleView;
         Button undoButton;
 
         ViewHolder(View itemView) {
             super(itemView);
 
+            reviewRatingView = (TextView) itemView.findViewById(R.id.review_rating);
             reviewTextView = (TextView) itemView.findViewById(R.id.review_text);
+            reviewTitleView = (TextView) itemView.findViewById(R.id.review_title);
             undoButton = (Button) itemView.findViewById(R.id.undo_button);
         }
     }
@@ -52,17 +58,33 @@ class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ViewHolder>
         // Get the data model based on the position
         mCursor.moveToPosition(position);
         String reviewTitle = mCursor.getString(mCursor.getColumnIndexOrThrow("review_title"));
+        String reviewText = mCursor.getString(mCursor.getColumnIndexOrThrow("review_text"));
         int rating = mCursor.getInt(mCursor.getColumnIndexOrThrow("star_rating"));
-
-        if (reviewTitle == null || reviewTitle.length() == 0) {
-            reviewTitle = "(none) rating = " + rating;
-        }
+        int id = mCursor.getInt(mCursor.getColumnIndexOrThrow("_id"));
 
         // Update the row with the details from this review
-        TextView textView = holder.reviewTextView;
-        textView.setText(reviewTitle);
+        TextView tempView = holder.reviewRatingView;
+        tempView.setText(Integer.toString(rating));
+
+        tempView = holder.reviewTitleView;
+
+        if (reviewTitle == null || reviewTitle.length() == 0) {
+            tempView.setText(R.string.none);
+        } else {
+            tempView.setText(reviewTitle);
+        }
+
+        tempView = holder.reviewTextView;
+        if (reviewText == null || reviewText.length() == 0) {
+            tempView.setText(R.string.none);
+        } else {
+            tempView.setText(reviewText);
+        }
+        
+        // Hide the undo button for now, once we've located it.
         Button button = holder.undoButton;
-        button.setText("UnDo"); // Needs translating, etc.
+        button.setVisibility(View.INVISIBLE);
+        button.setText(R.string.undo);
     }
 
     @Override
